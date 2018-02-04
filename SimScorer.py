6f28dbd2a2e68a4285a1cdc2497b3e4e9317f9e9
Ubuntu <jeff@docsim.ud2a1mwgwfhupbkydzacrsyr1h.ux.internal.cloudapp.net>
2018-02-04 10:58:27 +0000

    Make SimScorer insensitive to duplciate records. Add more specific words to stopwords.

--- SimScorer.py
+++ SimScorer.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import request, jsonify
-from scorer import computeScore
+from Scorer import computeScore
 
 app = Flask(__name__)
 
@@ -12,7 +12,13 @@ def score():
   usertitle = content['title']
   comparingTitle = len(usertitle) > 4
   userdescription = content['description']
+  seenTagLines = {}
   for record in content['candidates']:
+    if record['tagline'] in seenTagLines:
+      # Seen this exact record already, sort to the end
+      record['ideascore'] = 0
+      record['namescore'] = 0
+      continue
     if comparingTitle:
       # Compute name similarity and description similarity
       namescore = computeScore(usertitle, record['title'])
@@ -20,8 +26,10 @@ def score():
       record['namescore'] = namescore
       record['ideascore'] = ideascore
     else:
+      # Just compute description similarity
       ideascore = computeScore(userdescription, record['tagline'])
       record['ideascore'] = ideascore
+    seenTagLines[record['tagline']] = 1
   results = {}
   content['candidates'].sort(key=lambda x: x['ideascore'], reverse=True)
   results['idea'] = content['candidates'][:4]
